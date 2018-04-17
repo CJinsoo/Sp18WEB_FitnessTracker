de8025ba3e218784338dc893732a94e0b4107b6f
Jinsoo Choi <highgenius94@gmail.com>
2018-04-17 18:24:49 -0400

    4/17 6:30pm

---
+++ src/app/model/tracker.ts
@@ -0,0 +1,14 @@
+export class Profile{
+
+    isEdit:boolean = false;
+    name:string;
+    age:number;
+    gender:string;
+    email:string;
+    heightft:number;
+    heightin:number;
+    weight:number;
+    bmi:number;
+    goal:string;
+
+}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,23 +9,35 @@ export class ProfileComponent implements OnInit {
   isEdit:boolean = false;
   name:string;
   age:number;
+  gender:string;
   email:string;
-  bmi:number;
   heightft:number;
   heightin:number;
   weight:number;
+  bmi:number;
   goal:string;
 
   constructor() { 
     
   }
 
+  bmiCalculator(){
+    var height = this.heightft*12 + this.heightin;
+    var num = this.weight/height/height*703;
+    this.bmi = Math.round(num * 100) / 100;
+  }
+
   ngOnInit() {
     this.name = "Jane Doe";
+    this.age = 20;
     this.heightft = 5;
     this.heightin = 1;
+    this.weight = 100;
     this.email = "example@gmail.com";
+    
+    //this.bmi = this.bmiCalculator((this.heightft * 12 + this.heightin), this.weight);
   }
+  //this.weight / (this.heightft * 12 + this.heightin)*(this.heightft * 12 + this.heightin)) * 703
 
   toggleEdit(){
     this.isEdit = !this.isEdit;
